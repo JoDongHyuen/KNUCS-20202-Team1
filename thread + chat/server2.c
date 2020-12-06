fae56498701f88f4ae1961bb1747a15fdb5e6916
khg9849 <75154470+khg9849@users.noreply.github.com>
2020-12-06 14:59:44 +0900

    수정본2
    게임이 사용자에 의해서 돌아가게 해 놓았고 write, read명령어로 수정하면 될듯. 일단 버퍼가 문제인지는 모르겠지만 다음으로 넘어가는 부분이 이상해요오오

--- thread + chat/server2.c
+++ thread + chat/server2.c
@@ -23,7 +23,11 @@ FILE* users[8];
 int sock_id, client_fd;
 int read_cnt;
 char chat[BUFSIZ], *read_rt;
-
+//게임을 위해 추가한 변수
+int* user_roles;			//유저의 역할
+int* vote_users;			//유저별 투표수 저장
+int abstention = 0;			// 기권
+//
 
 void *waiting_room(void* nul)
 {
@@ -53,7 +57,52 @@ void *waiting_room(void* nul)
 		}
 	}
 }
+void make_user_roles(int u_roles[], int users) {
 
+	int map = 1;//마피아와 경찰 의사는 기본 1명
+	int pol = 1;
+	int doc = 1;
+	int civ;
+	int map_count = 0;
+	int pol_count = 0, doc_count = 0;
+	int temp = 0;
+
+	switch (users) {						//유저 수에 따라 마피아 수가 달라짐
+	case 6: map = 2; break;
+	case 7: map = 2; break;
+	case 8: map = 3; break;
+	}
+
+	civ = users - (map + pol + doc);		//시민은 남은 인원
+
+	srand((unsigned int)time(NULL));		//랜덤으로 분배
+
+	while (map_count < map) {				//마피아 먼저 정하기
+		temp = rand() % users + 1;
+		if (u_roles[temp] == 0) {
+			u_roles[temp] = -1;
+			map_count++;
+		}
+		else continue;
+	}
+
+	while (!pol_count || !doc_count) {		//의사와 경찰 정하기
+		temp = rand() % users + 1;			//if문 안에 있던 오류 수정
+		if (u_roles[temp] == 0 && pol_count == 0) {			
+			u_roles[temp] = 2;
+			pol_count = 1;
+		}
+		temp = rand() % users + 1;			//if문 안에 있던 오류 수정
+		if (u_roles[temp] == 0 && doc_count == 0) {			
+			u_roles[temp] = 3;
+			doc_count = 1;
+		}
+	}
+	for (int i = 1; i <= users; i++) {		//남은 인원들은 모두 시민
+		if (u_roles[i] == 0)
+			u_roles[i] = 1;
+	}
+}
 int main(int argc,char *argv[])
 {
 	
@@ -93,18 +142,26 @@ int main(int argc,char *argv[])
 	fcntl(sock_id,F_SETFL,flag | O_NONBLOCK);
 
 	pthread_create(&room, NULL, waiting_room, (void *)NULL);
+
+
+
+
 	while(1)
 	{
 		/*before game start*/
 		command = fgets();
 		if (strcmp(command, "start") == 0){
-
+			MALLOC(user_roles, num_user * sizeof(int));		//게임이 시작되면 유저 수 만큼 배열 할당
+			MALLOC(vote_users, users * sizeof(int));		//유저 수 만큼 투표 배열 할당
 		}
 
 		while(1)
 		{
 
 		/*직업 배정하는 파트*/
+		make_user_roles(user_roles, num_user);
+		/*유저의 직업을 알려주는 파트*/
+
 
 		/*낮이 되었습니다 파트*/
 
